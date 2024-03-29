b714ee3fdd3f5addac61f5805d77bc0233dc4686
Buddha <buddhalama88480@gmail.com>
2024-03-29 00:57:48 -0700

    added back button

--- frontend/src/pages/petsDescription/petsDescription.jsx
+++ frontend/src/pages/petsDescription/petsDescription.jsx
@@ -4,6 +4,7 @@ import Card from "../../components/card/card"
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import { IoIosArrowRoundBack } from "react-icons/io";
 
 
 export default function PetsDescription() {
@@ -11,7 +12,7 @@ export default function PetsDescription() {
     const [pet,setPet] = useState([])
     const {id} = useParams()
 
-    // http://localhost:3000/petFinder/selected/65ffebd27b6d832e34a03073
+   
 
   useEffect(()=>{
     axios
@@ -45,17 +46,24 @@ export default function PetsDescription() {
     return (
         <>
             <Nav />
+          
             <div className={styles.img_container}>
                 <img src={pet?.image?.url}className={styles.img_blur}></img>
                 <img src={pet?.image?.url} className={styles.img}></img>
+                <Link to = '/allPets' className={styles.link}>
+                <IoIosArrowRoundBack className={styles.backArrow}/>
+                </Link>
+
+               
             </div>
 
             <div className={styles.container}>
-
-
-
                 <div className={styles.description_container}>
+              
+                  
                     <h1 className={styles.heading}>{pet?.name}</h1>
+                
+
                     <div className={styles.buttons}>
                         <button className={`${styles.button} ${styles.button1}`}>
                             <p>Adopt Pet</p>
